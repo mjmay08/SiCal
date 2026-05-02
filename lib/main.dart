c804bd5d11d3063455e0628f5234b58ba2c7a078
Mitchell May <7408476+mjmay08@users.noreply.github.com>
2026-05-02 14:07:22 -0500

    Add Rust bridge integration with flutter_rust_bridge

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'src/app.dart';
+import 'src/bridge/sia_bridge.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SiaBridge.init();
   runApp(const SiCalApp());
 }
